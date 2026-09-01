705a80ae73d001bbfb97aa9e74d70f6e63c060c2
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-01 14:35:20 +0100

    style: fix recovery module lint

--- python/src/data_platform_lab/recovery/__init__.py
+++ python/src/data_platform_lab/recovery/__init__.py
@@ -1,5 +1,5 @@
 """End-to-end recovery orchestration for the local platform."""
 
-from data_platform_lab.recovery.pipeline import RecoveryResult, RecoverableIngestionPipeline
+from data_platform_lab.recovery.pipeline import RecoverableIngestionPipeline, RecoveryResult
 
 __all__ = ["RecoverableIngestionPipeline", "RecoveryResult"]
